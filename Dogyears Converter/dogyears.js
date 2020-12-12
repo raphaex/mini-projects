8a2eff8e117bac962cb7a45478fb5ecff2c36460
raphaex <72615828+raphaex@users.noreply.github.com>
2020-12-12 09:50:45 +0800

    can convert human years into dogyears

---
+++ Dogyears Converter/dogyears.js
@@ -0,0 +1,33 @@
+const myAge = 17
+//this is my age
+
+let earlyYears = 2
+earlyYears *= 10.5
+//since the first two years of dog's life is 10.5 years.
+
+let laterYears = myAge - 2
+laterYears = laterYears *= 4
+/*(this acocunts for the later years)
+This is for counting the remaining dog years. the 3rd to last years of the dog's life is 4x the human years.
+*/
+
+const myAgeInDogYears = earlyYears + laterYears
+/* this for adding the first two years and all the remaining years then (dog's years converted in human years.(var: myAge))
+*/
+
+const myName = 'raphaex.';
+console.log(myName.toLowerCase())
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+/* bonus quest 
+
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+console.log(earlyYears)
+
+let laterYears = myAge - 2
+laterYears = laterYears * 4
+console.log(laterYears)
+
+ */
