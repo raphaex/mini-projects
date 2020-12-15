cfbee3de154e08f3d7e3ef236982bd3e0e041f5e
raphaex <72615828+raphaex@users.noreply.github.com>
2020-12-15 12:33:09 +0800

    Delete magiceightQuest.js
    oofs wrong upload

--- magic eight ball/magiceightQuest.js
+++
@@ -1,4 +0,0 @@
-quest:
-
-1.In the first line of the program, define a variable called userName that is set to an empty string.
-If the user wants, he or she can enter his or her name in between the quotation marks.
