59ec79ee21fecf98b99ae593a59766ab03036f67
raphaex <72615828+raphaex@users.noreply.github.com>
2020-12-15 12:32:14 +0800

    Update sleepDebtCalculator.js
    added some practice comments explaining what the code was

--- Sleep Debt Calculator/sleepDebtCalculator.js
+++ Sleep Debt Calculator/sleepDebtCalculator.js
@@ -1,4 +1,6 @@
 /* bonus quest 
+alternative way of having the data of your sleep hours this can be paired with the alternative for
+getActualSleepHours() function. 
 
 const monday = 1
 const tuesday = 2
@@ -10,6 +12,8 @@ const  sunday = 7
 
 */
 
+
+//this is were you can add your sleep hours for the week
 function getSleepHours(day){
 	if(day === 'monday'){
 		return 8
@@ -36,9 +40,11 @@ function getSleepHours(day){
 
 function getActualSleepHours(){
 	return getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday')
-}
+}//the function that adds up all your sleep hours
 
 	/* bonus quest
+this is the alternative version of the function above this comment.	
+
 function getActualSleepHours(){
 	return monday + tuesday + wednesday + thursday + friday + saturday + sunday
 }
@@ -49,8 +55,13 @@ function getActualSleepHours(){
 function getIdealSleepHours(){
 	let idealHours = 8
 	return idealHours * 7
-}
+}//this is your ideal sleep hours 8 x 7 = 56 hours of sleep is needed to achieve an ideal week of sleep
+
+
 /*
+this is a alternative way of getting ideal hours you can input a parameter num and then that will serve as daily sleep goal
+ex: you input 7 then 7 * 7 = 49 hours of sleep to get an ideal hours of sleep
+
 function getIdealSleepHours(num){
 	let idealHours = num
 	return idealHours * 7
@@ -72,6 +83,8 @@ function calculateSleepDebt(){
 		return `your lacking exactly ${okay} sleep hours. go to bed now.`
 	}
 	
-}
+}//this function compares the actual sleep hours that you get from the ideal sleep hours and give you insights.
+
+
 
 console.log(calculateSleepDebt())
